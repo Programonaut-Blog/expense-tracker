1e7ef4accbdf12868e9be4325d343b322db931b5
Maximilian Kürschner <dev@maximilian-kuerschner.de>
2023-12-29 23:27:47 +0100

    fix: check for user not for valid token

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -25,12 +25,12 @@ export const authentication: Handle = async ({ event, resolve }) => {
     return response;
 }
 
-const unprotectedPrefix = ['/login'];
+const unprotectedPrefix = ['/login', '/register' ];
 export const authorization: Handle = async ({ event, resolve }) => {
 	// Protect any routes under /authenticated
 	if (!unprotectedPrefix.some((path) => event.url.pathname.startsWith(path)) && event.url.pathname !== '/') {
-		const session = await event.locals.pb.authStore.isValid;
-		if (!session) {
+		const loggedIn = await event.locals.pb.authStore;
+		if (!loggedIn) {
 			throw redirect(303, '/login');
 		}
 	}
