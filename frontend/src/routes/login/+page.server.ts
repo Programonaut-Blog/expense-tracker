59908818ffa608776a47ae5222660696fb5e1b61
Maximilian Kürschner <dev@maximilian-kuerschner.de>
2023-12-29 23:36:02 +0100

    feat: email-password auth

---
+++ frontend/src/routes/login/+page.server.ts
@@ -0,0 +1,53 @@
+import { fail, redirect } from '@sveltejs/kit';
+import type { ClientResponseError } from 'pocketbase';
+import type { PageServerLoad } from './$types';
+
+export const load = (async ({locals}) => {    
+    if (locals.pb.authStore) {
+        return redirect(303, '/dashboard')
+    }
+
+    return {};
+}) satisfies PageServerLoad;
+
+export const actions = {
+    register: async ({ locals, request }) => {
+		const data = await request.formData();
+        const email = data.get('email');
+        const password = data.get('password');
+        
+        if (!email || !password) {
+            return fail(400, { emailRequired: email === null, passwordRequired: password === null });
+        }
+		
+        data.set('passwordConfirm', password?.toString())        
+		try {
+			await locals.pb.collection('users').create(data);
+			await locals.pb.collection('users').authWithPassword(email, password.toString());
+			await locals.pb.collection('users').requestVerification(email);
+		} catch (error) {
+			const errorObj = error as ClientResponseError;
+            return fail(500, {fail: true, message: errorObj.data.message});
+        } 
+		
+		throw redirect(303, '/dashboard');
+	},
+    login: async ({ locals, request }) => {
+        const data = await request.formData();
+        const email = data.get('email');
+        const password = data.get('password');
+        
+        if (!email || !password) {
+            return fail(400, { emailRequired: email === null, passwordRequired: password === null });
+        }
+
+        try {
+            await locals.pb.collection('users').authWithPassword(email.toString(), password.toString());
+        } catch (error) {
+            const errorObj = error as ClientResponseError;
+            return fail(500, {fail: true, message: errorObj.data.message});
+        }
+
+        throw redirect(303, '/dashboard');
+    }
+}
---
+++ frontend/src/routes/login/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import type { ActionData, PageData } from './$types';
+    
+    export let data: PageData;
+	export let form: ActionData;
+</script>
+
+<div class="h-full flex flex-col justify-center sm:mx-auto sm:w-full sm:max-w-sm">
+    {#if form?.fail}
+        <div class="variant-soft-error px-4 py-2 mb-2 rounded-token">
+            {form.message}
+        </div>
+    {/if}
+    <form action="?/login" method="post">
+        <label class="label">
+            <span>E-Mail</span>
+            <input class="input" name="email" title="E-Mail" type="email" placeholder="mail@example.com" required />
+        </label>
+        <label class="label mt-4">
+            <span>Password</span>
+            <input class="input" name="password" title="Password" type="password" placeholder="******" required />
+        </label>
+        <button class="btn variant-filled w-full mt-4" type="submit">Login</button>
+        <button class="btn variant-filled w-full mt-4 mb-2" formaction="?/register">Register</button>
+        <hr />
+        <button class="btn variant-filled w-full mt-2" formaction="?/register">Google</button>
+    </form>
+</div>
