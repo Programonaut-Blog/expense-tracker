ebce0aa005c655fbb363e8feb8507d0cf7ec92b7
ian <5948446+jorgemxm@users.noreply.github.com>
2024-02-09 13:49:06 -0500

    fix: Remove unnecessary await for PB sync methods and PB object property

--- frontend/src/routes/login/+page.server.ts
+++ frontend/src/routes/login/+page.server.ts
@@ -79,7 +79,7 @@ export const actions = {
         throw redirect(303, provider?.authUrl + env.REDIRECT_URL + provider?.name);
     },
     logout: async ({ locals }) => {
-        await locals.pb.authStore.clear();
+        locals.pb.authStore.clear();
         throw redirect(303, '/login');
     }
 } satisfies Actions;
